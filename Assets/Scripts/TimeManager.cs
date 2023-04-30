150ec32313fbf60b08771de80dd8eea14e4a9f46
Jayden WIllams <jay.i.will2020@gmail.com>
2023-04-30 12:50:32 -0700

    TimerScript

---
+++ Assets/Scripts/TimeManager.cs
@@ -0,0 +1,50 @@
+﻿﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{ 
+    public bool IsFinite { get; private set; }
+    public float TotalTime { get; private set; }
+    public float TimeRemaining { get; private set; }
+    public bool IsOver { get; private set; }
+
+
+    private void Awake()
+    {
+        IsFinite = false;
+        TimeRemaining = TotalTime;
+    }
+
+
+    void OnEnable()
+    {
+        OnAdjustTime += AdjustTime;
+        OnSetTime += SetTime;
+    }
+
+    private void OnDisable()
+    {
+        OnAdjustTime -= AdjustTime;
+        OnSetTime -= SetTime;
+    }
+
+    private void AdjustTime(float delta)
+    {
+        TimeRemaining += delta;
+    }
+
+    private void SetTime(int time, bool isFinite, GameMode gameMode)
+    {
+        TotalTime = time;
+        IsFinite = isFinite;
+        TimeRemaining = TotalTime;
+    }
+
+    void Update()
+    {
+
+    }
+}
+
