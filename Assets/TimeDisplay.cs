f2ead1e774defbfafdb2db630c29620b94eee6a7
Jayden WIllams <78007992+2011-williams-jayden@users.noreply.github.com>
2023-04-30 12:53:46 -0700

    Add files via upload

---
+++ Assets/TimeDisplay.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KartGame.Track
+{
+    public class TimeDisplay : MonoBehaviour
+    {
+        
+        [Tooltip("Pool object for the time display UI item.")]
+        public ObjectDef timeDisplayItem;
+
+
+        void Awake()
+        {
+            currentTime = 0;
+            lapsOver = false;
+        }
+
+        void OnEnable()
+        {
+
+        }
+
+        TimeDisplayItem GetItem(int i)
+        {
+
+            if (i >= lapTimesText.Count)
+            {
+                TimeDisplayItem newItem = timeDisplayItem.getObject(false, finishedLapsParent.transform).GetComponent<TimeDisplayItem>();
+                finishedLapsParent.UpdateTable(newItem.gameObject);
+                lapTimesText.Add(newItem);
+                return newItem;
+            }
+
+            return lapTimesText[i];
+        }
+
+
+        void Update()
+        {
+            if (currentTime == 0) return;
+            if (levelClear) return;
+            currentLapText.SetText(DisplayCurrentTime());
+        }
+
+        string DisplayCurrentTime()
+        {
+            float currentTime = Time.time - newCurrentTime;
+            if (currentTime < 0.01f) return "0:00";
+            return getTimeString(currentTime);
+        }
+    
+        string getTimeString(float time)
+        {
+            int timeInt = (int)(time);
+            int minutes = timeInt / 60;
+            int seconds = timeInt % 60;
+            float fraction = (time * 100) % 100;
+            if (fraction > 99) fraction = 99;
+            return string.Format("{0}:{1:00}:{2:00}", minutes, seconds, fraction);
+        }
+    }
+}
---
+++ Assets/TimeDisplayItem.cs
@@ -0,0 +1,31 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class TimeDisplayItem : MonoBehaviour
+{
+    [Tooltip("A reference to the TextMeshProUGUI to display the time.")]
+    [SerializeField]
+    protected TextMeshProUGUI display;
+
+    [Tooltip("A reference to the TextMeshProUGUI to display the title for the time.")]
+    [SerializeField]
+    protected TextMeshProUGUI title;
+    
+    public void SetText(string text)
+    {
+        if (string.IsNullOrEmpty(text))
+        {
+            gameObject.SetActive(false);
+            return;
+        }
+        
+        gameObject.SetActive(true);
+        display.text = text;
+    }
+
+    public void SetTitle(string text)
+    {
+        title.text = text;
+    }
+
+}
